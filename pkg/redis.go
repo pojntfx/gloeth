9a64cdced0f09b54b963e92b5623dbdc97aeca77
Felicitas Pojtinger <felicitas@pojtinger.com>
2019-12-27 18:01:15 +0100

    feat: Enable broadcast

--- pkg/redis.go
+++ pkg/redis.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"github.com/go-redis/redis/v7"
+	"strings"
 )
 
 type Redis struct {
@@ -17,9 +18,37 @@ func (d *Redis) Connect(hostPort, password string) {
 }
 
 func (d *Redis) RegisterNode(macAddress, tcpReadHostPort string) error {
-	return d.client.Set(macAddress, tcpReadHostPort, 0).Err()
+	return d.client.Set("node+"+macAddress, tcpReadHostPort, 0).Err()
 }
 
-func (d *Redis) GetTcpReadHostPortForNode(macAddress string) (string, error) {
-	return d.client.Get(macAddress).Result()
+func (d *Redis) getTcpReadHostPortForMacAddress(macAddress string) (string, error) {
+	return d.client.Get("node+" + macAddress).Result()
+}
+
+func (d *Redis) GetTcpReadHostPortsForMacAddress(macAddress string) ([]string, error) {
+	if strings.TrimSpace(macAddress) == "ff:ff:ff:ff:ff:ff" {
+		macAddresses, err := d.client.Keys("node+*").Result()
+		if err != nil {
+			return []string{}, err
+		}
+
+		var tcpReadHostPorts []string
+		for _, macAddress := range macAddresses {
+			tcpReadHostPort, err := d.getTcpReadHostPortForMacAddress(strings.Replace(macAddress, "node+", "", -1))
+			if err != nil {
+				return []string{}, err
+			}
+
+			tcpReadHostPorts = append(tcpReadHostPorts, tcpReadHostPort)
+		}
+
+		return tcpReadHostPorts, nil
+	}
+
+	tcpReadHostPort, err := d.getTcpReadHostPortForMacAddress(macAddress)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return []string{tcpReadHostPort}, nil
 }
