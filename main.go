2567df7c76ac2645a7067536279a4227544be894
Felix Pojtinger <felix@pojtinger.com>
2020-03-23 00:00:02 +0100

    feat: Add minimal re-implementation bsed on tinytaptunnel

--- main.go
+++ main.go
@@ -1,91 +1,220 @@
+// Based on https://github.com/vsergeev/tinytaptunnel
+
 package main
 
 import (
+	"encoding/binary"
+	"errors"
 	"flag"
-	redisLib "github.com/go-redis/redis/v7"
-	"github.com/pojntfx/gloeth/pkg"
+	"fmt"
 	"log"
+	"net"
+	"syscall"
+	"time"
+	"unsafe"
 )
 
-func main() {
-	var (
-		tapName         = flag.String("device", "gloeth0", "Name of the network device to create")
-		tcpReadHostPort = flag.String("listen", "127.0.0.1:1234", "Host:port to listen on")
+const (
+	TIMESTAMP_SIZE    = 8               // TIMESTAMP_SIZE is the size of the timestamp
+	TIMESTAMP_TIMEOUT = time.Second * 3 // TIMESTAMP_TIMEOUT is the maximum duration after which a frame will be discarded
 
-		redisHostPort = flag.String("redis-host", "127.0.0.1:6379", "Host:port of Redis")
-		redisPassword = flag.String("redis-password", "", "Password for Redis")
-	)
-	flag.Parse()
+	UDP_MTU = 1472                          // The UDP MTU
+	TAP_MTU = UDP_MTU - 14 - TIMESTAMP_SIZE // TAP_MTU is UDP_MTU - ethernet header (14) - TIMESTAMP_SIZE
+)
 
-	var (
-		tapErrorChan = make(chan error)
-		tcpErrorChan = make(chan error)
+// EncapsulateFrame encapsulates a frame
+// A frame is composed of a nanosecond timestamp (8 bytes) and a plaintext frame (1-1432 bytes)
+func EncapsulateFrame(frame []byte) ([]byte, error) {
+	timeInByte := make([]byte, 8)
+	binary.BigEndian.PutUint64(timeInByte, uint64(time.Now().UnixNano()))
 
-		tapStatusChan = make(chan string)
-		tcpStatusChan = make(chan string)
+	return append(timeInByte, frame...), nil
+}
 
-		tapReadFramesChan = make(chan []byte)
-		tcpReadFramesChan = make(chan []byte)
-	)
-
-	redis := pkg.Redis{}
-
-	redis.Connect(*redisHostPort, *redisPassword)
-
-	tap := pkg.TAP{
-		Name: *tapName,
+// DecapsulateFrame decapsulates a frame
+func DecapsulateFrame(frame []byte) ([]byte, error) {
+	if len(frame) < (TIMESTAMP_SIZE + 1) {
+		return nil, errors.New("invalid encapsulated frame size")
 	}
 
-	if err := tap.Init(); err != nil {
-		log.Fatalln("TAP init error:", err)
+	timeInByte := frame[0:TIMESTAMP_SIZE]
+	timeInUnixNano := int64(binary.BigEndian.Uint64(timeInByte))
+	if (time.Now().UnixNano() - timeInUnixNano) > TIMESTAMP_TIMEOUT.Nanoseconds() {
+		return nil, errors.New("timestamp out of acceptable range")
 	}
 
-	err, macAddress := tap.GetMacAddress()
+	return frame[TIMESTAMP_SIZE:], nil
+}
+
+// TAPDevice is a TAP device
+type TAPDevice struct {
+	fd      int
+	devName string
+}
+
+// Open opens the TAP device
+func (t *TAPDevice) Open(mtu uint) error {
+	fd, err := syscall.Open("/dev/net/tun", syscall.O_RDWR, syscall.S_IRUSR|syscall.S_IWUSR|syscall.S_IRGRP|syscall.S_IROTH)
 	if err != nil {
-		log.Fatalln("TAP registration error:", err)
+		return fmt.Errorf("could not open /dev/net/tun: %v", err)
+	}
+	t.fd = fd
+
+	ifreqFlags := uint16(syscall.IFF_TAP | syscall.IFF_NO_PI)
+	ifreq := make([]byte, 32)
+	ifreq[16] = byte(ifreqFlags)
+	ifreq[17] = byte(ifreqFlags >> 8)
+	currentFlags, _, err := syscall.Syscall(syscall.SYS_IOCTL, uintptr(t.fd), syscall.TUNSETIFF, uintptr(unsafe.Pointer(&ifreq[0])))
+	if currentFlags != 0 {
+		t.Close()
+
+		return fmt.Errorf("could not set tun/tap type: %v", err)
 	}
 
-	if err := redis.RegisterNode(macAddress, *tcpReadHostPort); err != nil {
-		log.Fatalln("TAP registration error:", err)
+	t.devName = string(ifreq[0:16])
+
+	rawSocketFd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
+	if err != nil {
+		t.Close()
+
+		return fmt.Errorf("could not create packet socket: %v", err)
+	}
+	defer syscall.Close(rawSocketFd)
+
+	err = syscall.BindToDevice(rawSocketFd, t.devName)
+	if err != nil {
+		t.Close()
+
+		return fmt.Errorf("could not bind packet socket to TAP device: %v", err)
 	}
 
-	tcp := pkg.TCP{
-		ReadHostPort: *tcpReadHostPort,
+	ifreqMTU := mtu
+	ifreq[16] = byte(ifreqMTU)
+	ifreq[17] = byte(ifreqMTU >> 8)
+	ifreq[18] = byte(ifreqMTU >> 16)
+	ifreq[19] = byte(ifreqMTU >> 24)
+	currentFlags, _, err = syscall.Syscall(syscall.SYS_IOCTL, uintptr(rawSocketFd), syscall.SIOCSIFMTU, uintptr(unsafe.Pointer(&ifreq[0])))
+	if currentFlags != 0 {
+		t.Close()
+
+		return fmt.Errorf("could not set MTU on TAP device: %v", err)
 	}
 
-	go tap.Read(tapErrorChan, tapStatusChan, tapReadFramesChan)
-	go tcp.Read(tcpErrorChan, tcpStatusChan, tcpReadFramesChan)
+	currentFlags, _, err = syscall.Syscall(syscall.SYS_IOCTL, uintptr(rawSocketFd), syscall.SIOCGIFFLAGS, uintptr(unsafe.Pointer(&ifreq[0])))
+	if currentFlags != 0 {
+		t.Close()
+
+		return fmt.Errorf("could not get TAP device flags: %v", err)
+	}
+
+	ifreqFlags = uint16(ifreq[16]) | (uint16(ifreq[17]) << 8)
+	ifreqFlags |= syscall.IFF_UP | syscall.IFF_RUNNING
+	ifreq[16] = byte(ifreqFlags)
+	ifreq[17] = byte(ifreqFlags >> 8)
+	currentFlags, _, err = syscall.Syscall(syscall.SYS_IOCTL, uintptr(rawSocketFd), syscall.SIOCSIFFLAGS, uintptr(unsafe.Pointer(&ifreq[0])))
+	if currentFlags != 0 {
+		t.Close()
+
+		return fmt.Errorf("could not bring up TAP device: %v", err)
+	}
+
+	return nil
+}
+
+// Close closes the TAP device
+func (t *TAPDevice) Close() {
+	syscall.Close(t.fd)
+}
+
+// Read reads from the TAP device
+func (t *TAPDevice) Read(b []byte) (int, error) {
+	return syscall.Read(t.fd, b)
+}
+
+// Write writes to the TAP device
+func (t *TAPDevice) Write(b []byte) (n int, err error) {
+	return syscall.Write(t.fd, b)
+}
+
+// ForwardUDPtoTAP forwards UDP packets to a TAP device
+func ForwardUDPtoTAP(udpConn *net.UDPConn, tapDevice *TAPDevice, remoteAddr *net.UDPAddr) {
+	log.Printf("forwarding UDP to TAP with remote %v:%v\n", remoteAddr.IP, remoteAddr.Port)
 
 	for {
-		select {
-		case err := <-tapErrorChan:
-			log.Println("TAP error:", err)
-		case err := <-tcpErrorChan:
-			log.Println("TCP error:", err)
+		packet := make([]byte, UDP_MTU)
+		var frame []byte
 
-		case status := <-tapStatusChan:
-			log.Println("TAP status:", status)
-		case status := <-tcpStatusChan:
-			log.Println("TCP status:", status)
+		n, _, err := udpConn.ReadFromUDP(packet)
+		if err != nil {
+			log.Fatalf("could not read from UDP socket: %v\n", err)
+		}
 
-		case frame := <-tapReadFramesChan:
-			err, macAddress := pkg.GetDestinationMacAddressFromFrame(frame)
-			if err != nil {
-				log.Println("SWITCH error:", err)
-			}
+		frame, invalid := DecapsulateFrame(packet[0:n])
+		if invalid != nil {
+			continue
+		}
 
-			destinations, err := redis.GetTcpReadHostPortsForMacAddress(macAddress)
-			if err == redisLib.Nil {
-				log.Println("SWITCH error: could not find node with mac address", macAddress, err)
-			} else if err != nil {
-				log.Println("SWITCH error:", err)
-			} else {
-				for _, destination := range destinations {
-					go tcp.Write(tcpErrorChan, tcpStatusChan, frame, destination)
-				}
-			}
-		case frame := <-tcpReadFramesChan:
-			go tap.Write(tapErrorChan, tapStatusChan, frame)
+		_, err = tapDevice.Write(frame)
+		if err != nil {
+			log.Fatalf("could not write to TAP device: %v\n", err)
 		}
 	}
+}
+
+// ForwardTAPtoUDP forwards frames from a TAP device to a UDP connection
+func ForwardTAPtoUDP(udpConn *net.UDPConn, tapDevice *TAPDevice, remoteAddr *net.UDPAddr) {
+	log.Printf("forwarding TAP to UDP with remote %v:%v\n", remoteAddr.IP, remoteAddr.Port)
+
+	for {
+		frame := make([]byte, TAP_MTU+14)
+		var encFrame []byte
+
+		n, err := tapDevice.Read(frame)
+		if err != nil {
+			log.Fatalf("could not read from TAP device: %v\n", err)
+		}
+
+		encFrame, invalid := EncapsulateFrame(frame[0:n])
+		if invalid != nil {
+			continue
+		}
+
+		_, err = udpConn.WriteToUDP(encFrame, remoteAddr)
+		if err != nil {
+			log.Fatalf("could not write to UDP socket: %v\n", err)
+		}
+	}
+}
+
+func main() {
+	localAddrFlag := flag.String("localAddr", "0.0.0.0:1234", "Local address")
+	remoteAddrFlag := flag.String("remoteAddr", "0.0.0.0:12345", "Remote address")
+
+	flag.Parse()
+
+	localAddr, err := net.ResolveUDPAddr("udp", *localAddrFlag)
+	if err != nil {
+		log.Fatalf("could not resolve local address: %v\n", err)
+	}
+
+	remoteAddr, err := net.ResolveUDPAddr("udp", *remoteAddrFlag)
+	if err != nil {
+		log.Fatalf("could not resolve remote address: %v\n", err)
+	}
+
+	udpConn, err := net.ListenUDP("udp", localAddr)
+	if err != nil {
+		log.Fatalf("error creating a UDP socket: %v\n", err)
+	}
+
+	tapDev := new(TAPDevice)
+	err = tapDev.Open(TAP_MTU)
+	if err != nil {
+		log.Fatalf("could not open a TAP device: %v\n", err)
+	}
+
+	log.Printf("started tunnel with TAP device %v", tapDev.devName)
+
+	go ForwardUDPtoTAP(udpConn, tapDev, remoteAddr)
+	ForwardTAPtoUDP(udpConn, tapDev, remoteAddr)
 }
