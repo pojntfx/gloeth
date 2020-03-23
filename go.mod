d7bf01f07b70d815cdcfe44bb544a81a075da09b
Felix Pojtinger <felix@pojtinger.com>
2020-03-23 14:12:24 +0100

    feat: Use songgao/water and vishvananda/netlink

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/pojntfx/gloeth
 
 go 1.14
+
+require (
+	github.com/songgao/water v0.0.0-20200317203138-2b4b6d7c09d8
+	github.com/vishvananda/netlink v1.1.0
+	golang.org/x/sys v0.0.0-20200321134203-328b4cd54aae // indirect
+)
--- pkg/v2/pkg/devices/tap.go
+++ pkg/v2/pkg/devices/tap.go
@@ -1,99 +1,55 @@
 package devices
 
 import (
-	"syscall"
-	"unsafe"
-
 	"github.com/pojntfx/gloeth/pkg/v2/pkg/constants"
+	"github.com/songgao/water"
+	"github.com/vishvananda/netlink"
 )
 
 // TAPDevice is a TAP device
 type TAPDevice struct {
-	mtu        uint
+	mtu        int
 	framesChan chan []byte
-	fd         int
+	iface      *water.Interface
+	name       string
 }
 
 // NewTAPDevice creates a new TAP device
-func NewTAPDevice(mtu uint, framesChan chan []byte) *TAPDevice {
+func NewTAPDevice(mtu int, name string, framesChan chan []byte) *TAPDevice {
 	return &TAPDevice{
 		mtu:        mtu,
+		name:       name,
 		framesChan: framesChan,
 	}
 }
 
 // Open opens the TAP device
 func (t *TAPDevice) Open() error {
-	fd, err := syscall.Open("/dev/net/tun", syscall.O_RDWR, syscall.S_IRUSR|syscall.S_IWUSR|syscall.S_IRGRP|syscall.S_IROTH)
+	dev, err := water.New(water.Config{DeviceType: water.TAP, PlatformSpecificParams: water.PlatformSpecificParams{Name: t.name}})
 	if err != nil {
 		return err
 	}
-	t.fd = fd
 
-	ifreqFlags := uint16(syscall.IFF_TAP | syscall.IFF_NO_PI)
-	ifreq := make([]byte, 32)
-	ifreq[16] = byte(ifreqFlags)
-	ifreq[17] = byte(ifreqFlags >> 8)
-	currentFlags, _, err := syscall.Syscall(syscall.SYS_IOCTL, uintptr(t.fd), syscall.TUNSETIFF, uintptr(unsafe.Pointer(&ifreq[0])))
-	if currentFlags != 0 {
-		t.Close()
-
-		return err
-	}
-
-	devName := string(ifreq[0:16])
-
-	rawSocketFd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
+	link, err := netlink.LinkByName(t.name)
 	if err != nil {
-		t.Close()
-
 		return err
 	}
-	defer syscall.Close(rawSocketFd)
-
-	err = syscall.BindToDevice(rawSocketFd, devName)
-	if err != nil {
-		t.Close()
-
+	if err := netlink.LinkSetMTU(link, t.mtu); err != nil {
 		return err
 	}
 
-	ifreqMTU := t.mtu
-	ifreq[16] = byte(ifreqMTU)
-	ifreq[17] = byte(ifreqMTU >> 8)
-	ifreq[18] = byte(ifreqMTU >> 16)
-	ifreq[19] = byte(ifreqMTU >> 24)
-	currentFlags, _, err = syscall.Syscall(syscall.SYS_IOCTL, uintptr(rawSocketFd), syscall.SIOCSIFMTU, uintptr(unsafe.Pointer(&ifreq[0])))
-	if currentFlags != 0 {
-		t.Close()
-
+	if err := netlink.LinkSetUp(link); err != nil {
 		return err
 	}
 
-	currentFlags, _, err = syscall.Syscall(syscall.SYS_IOCTL, uintptr(rawSocketFd), syscall.SIOCGIFFLAGS, uintptr(unsafe.Pointer(&ifreq[0])))
-	if currentFlags != 0 {
-		t.Close()
-
-		return err
-	}
-
-	ifreqFlags = uint16(ifreq[16]) | (uint16(ifreq[17]) << 8)
-	ifreqFlags |= syscall.IFF_UP | syscall.IFF_RUNNING
-	ifreq[16] = byte(ifreqFlags)
-	ifreq[17] = byte(ifreqFlags >> 8)
-	currentFlags, _, err = syscall.Syscall(syscall.SYS_IOCTL, uintptr(rawSocketFd), syscall.SIOCSIFFLAGS, uintptr(unsafe.Pointer(&ifreq[0])))
-	if currentFlags != 0 {
-		t.Close()
-
-		return err
-	}
+	t.iface = dev
 
 	return nil
 }
 
 // Close closes the TAP device
 func (t *TAPDevice) Close() error {
-	return syscall.Close(t.fd)
+	return t.iface.Close()
 }
 
 // Read reads from the TAP device
@@ -101,7 +57,7 @@ func (t *TAPDevice) Read() error {
 	for {
 		frame := make([]byte, constants.FRAME_SIZE)
 
-		_, err := syscall.Read(t.fd, frame)
+		_, err := t.iface.Read(frame)
 		if err != nil {
 			return err
 		}
@@ -112,5 +68,5 @@ func (t *TAPDevice) Read() error {
 
 // Write writes to the TAP device
 func (t *TAPDevice) Write(frame []byte) (int, error) {
-	return syscall.Write(t.fd, frame)
+	return t.iface.Write(frame)
 }
