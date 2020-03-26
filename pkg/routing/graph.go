c2ecc2110e0abba02f8a1dd86a97b1b5cdcb1a98
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-03-26 20:14:27 +0100

    refactor: Decompose `GetGraphFromRawData` and `GetRawDataFromGraph` funcs, add unit test for the former

---
+++ pkg/routing/graph.go
@@ -0,0 +1,46 @@
+package routing
+
+import (
+	"fmt"
+
+	"github.com/sauerbraten/graph/v2"
+)
+
+// GetGraphFromRawData gets a graph from a multidimensional array (also see GetRawDataFromGraph)
+func GetGraphFromRawData(in [][2]string) *graph.Graph {
+	g := graph.New()
+
+	edges := DeduplicateNestedArray(in)
+	nodes := GetUniqueKeys(edges)
+
+	fmt.Println(len(in), len(nodes), len(edges))
+
+	for _, node := range nodes {
+		g.Add(node)
+	}
+
+	for _, edge := range edges {
+		g.Connect(edge[0], edge[1], 1)
+	}
+
+	return g
+}
+
+// GetRawDataFromGraph gets a multidimensional array (also see GetGraphFromRawData)
+func GetRawDataFromGraph(in *graph.Graph) [][2]string {
+	nodes := in.GetAll()
+
+	nodeMap := make(map[string]*graph.Node)
+	for _, node := range nodes {
+		nodeMap[node.Key()] = node
+	}
+
+	var nodesWithNeighborKeys [][2]string
+	for nodeKey, node := range nodeMap {
+		for neighbor := range node.GetNeighbors() {
+			nodesWithNeighborKeys = append(nodesWithNeighborKeys, [2]string{nodeKey, neighbor.Key()})
+		}
+	}
+
+	return DeduplicateNestedArray(nodesWithNeighborKeys)
+}
