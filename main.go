86f0ae4cb127138c88e2ff23a8937f3832f8c34a
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-09-06 23:00:02 +0200

    refactor: Don't destinguish between local and remote key for now

--- main.go
+++ main.go
@@ -7,17 +7,16 @@ import (
 
 func main() {
 	deviceName := *flag.String("deviceName", "gloeth0", "Network device name")
+	token := *flag.String("preSharedKey", "supersecretkey", "Pre-shared key")
 
 	localAddr := *flag.String("localAddr", "0.0.0.0:1927", "Local address")
 	localCert := *flag.String("localCert", "local.crt", "Local certificate")
 	localKey := *flag.String("localKey", "local.key", "Local key")
-	localToken := *flag.String("localToken", "supersecrettoken", "Local token")
 
 	remoteAddr := *flag.String("remoteAddr", "10.0.0.25:1927", "Remote address")
 	remoteCert := *flag.String("remoteCert", "remote.crt", "Remote certificate")
-	remoteToken := *flag.String("remoteToken", "supersecrettoken", "Remote token")
 
 	flag.Parse()
 
-	log.Println(deviceName, localAddr, localCert, localKey, localToken, remoteAddr, remoteCert, remoteToken)
+	log.Println(deviceName, token, localAddr, localCert, localKey, remoteAddr, remoteCert)
 }
