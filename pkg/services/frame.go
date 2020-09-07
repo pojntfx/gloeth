05eb90743cf5e3d3135e875e27d1eae3af45eb2b
Felix Pojtinger <felix@pojtinger.com>
2020-09-07 19:35:32 +0200

    feat: Add debugging output and get MVP to work

--- pkg/services/frame.go
+++ pkg/services/frame.go
@@ -1,6 +1,8 @@
 package services
 
-import proto "github.com/pojntfx/gloeth/pkg/proto/generated"
+import (
+	proto "github.com/pojntfx/gloeth/pkg/proto/generated"
+)
 
 //go:generate sh -c "mkdir -p ../proto/generated && protoc --go_out=paths=source_relative,plugins=grpc:../proto/generated -I=../proto ../proto/*.proto"
 
@@ -14,17 +16,29 @@ func NewFrameService() *FrameService {
 }
 
 func (s *FrameService) TransceiveFrames(channel proto.FrameService_TransceiveFramesServer) error {
+	wait := make(chan struct{})
+
 	s.channel = channel
 
-	for {
+	<-wait // Wait forever
 
-	}
+	return nil
 }
 
 func (s *FrameService) Write(frame *proto.FrameMessage) error {
+	s.waitTillOpen()
+
 	return s.channel.Send(frame)
 }
 
 func (s *FrameService) Read() (*proto.FrameMessage, error) {
+	s.waitTillOpen()
+
 	return s.channel.Recv()
+}
+
+func (s *FrameService) waitTillOpen() {
+	for s.channel == nil {
+
+	}
 }
